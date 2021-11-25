0a2d333b334e4ed6b7572b533a8039e23dbf24a2
danofff <danofff@mail.ru>
2021-11-24 22:40:07 -0600

    main game engine is finished

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //some DOM elements
 const playersTurn = document.querySelector("#players-turn");
+const resetButton = document.querySelector('#reset');
 
 const gameState = {
   players: ["player1", "player2"],
@@ -7,7 +8,8 @@ const gameState = {
   rowsQuantity: 6,
   columnsQuantity: 7,
   allDomCols: [],
-  positionsToWinQuantity: 4
+  positionsToWinQuantity: 4,
+  isGameFinished: false,
 };
 
 initiateGame();
@@ -17,14 +19,20 @@ function generateNewBoard() {
   for (let i = 0; i < gameState.columnsQuantity; i++) {
     board.push([]);
   }
-
   return board;
 }
 
 function initiateGame() {
+  //initiate start game state
   gameState.board = generateNewBoard();
   gameState.turn = false;
+  isGameFinished = false;
   gameState.allDomCols = document.querySelectorAll(".column");
+
+  //delete and set eventListener to the reset button
+  resetButton.removeEventListener('click', resetButtonClick);
+  resetButton.addEventListener('click', resetButtonClick);
+
   onBoardClick();
 }
 
@@ -37,28 +45,34 @@ function onBoardClick() {
 }
 
 function handleColumnClick(event) {
-
+  if (gameState.isGameFinished) {
+    return;
+  }
   //identify clicked column number;
   let clickedColumnNumber = event.target.closest(".column").id.split("-")[1];
 
-  //identify availiable row number and push player number to columns array
-  if(gameState.board[clickedColumnNumber - 1].length >= 6) {
+  //check if column has space for step
+  if (gameState.board[clickedColumnNumber - 1].length >= 6) {
     return;
   }
+
+  //identify availiable row number and push player number to columns array
   let filledRowNumber =
     gameState.rowsQuantity -
     gameState.board[clickedColumnNumber - 1].push(gameState.turn) +
     1;
 
+  //set style for DOM element
   let chosenElement = document.querySelector(
     `#column-${clickedColumnNumber} .row-${filledRowNumber}`
   );
   chosenElement.classList.add(gameState.players[+gameState.turn]);
 
   //checking for the win
-  if(checkForWin()) {
+  if (checkForWin()) {
     freezeTheGame();
-    playersTurn.innerText = `Player ${+gameState.turn + 1} is a WINNER`;
+    playersTurn.innerText = `Player ${+gameState.turn + 1} is a WINNER!!!`;
+    gameState.turn ? playersTurn.classList.add('winner-yellow'): playersTurn.classList.add('winner-red');
     return;
   }
 
@@ -68,15 +82,28 @@ function handleColumnClick(event) {
 }
 
 function checkForWin() {
-  return checkAllColumns() || checkAllRows();
+  return checkAllColumns() || checkAllRows() || checkAllDiagonels();
 }
 
 function checkAllColumns() {
   let result = false;
-  gameState.board.forEach(column => {
-    if(column.length >= gameState.positionsToWinQuantity) {
-      for(let i = 0; i <= column.length - (gameState.rowsQuantity - gameState.positionsToWinQuantity); i++) {
-        if(checkFourForWin(column[i], column[i+1], column[i+2], column[i+3])) {
+  gameState.board.forEach((column) => {
+    if (column.length >= gameState.positionsToWinQuantity) {
+      for (
+        let i = 0;
+        i <=
+        column.length -
+          (gameState.rowsQuantity - gameState.positionsToWinQuantity);
+        i++
+      ) {
+        if (
+          checkFourForWin(
+            column[i],
+            column[i + 1],
+            column[i + 2],
+            column[i + 3]
+          )
+        ) {
           result = true;
         }
       }
@@ -87,15 +114,54 @@ function checkAllColumns() {
 
 function checkAllRows() {
   let result = false;
-  for(let i = 0; i < gameState.rowsQuantity; i++) {
-    for(let j = 0; j <= gameState.columnsQuantity - gameState.positionsToWinQuantity; j++) {
-      
+  for (let i = 0; i < gameState.rowsQuantity; i++) {
+    for (
+      let j = 0;
+      j <= gameState.columnsQuantity - gameState.positionsToWinQuantity;
+      j++
+    ) {
       let one = gameState.board[j][i];
       let two = gameState.board[j + 1][i];
       let tree = gameState.board[j + 2][i];
       let four = gameState.board[j + 3][i];
 
-      if(checkFourForWin(one, two, tree, four)) {
+      if (checkFourForWin(one, two, tree, four)) {
+        result = true;
+      }
+    }
+  }
+  return result;
+}
+
+function checkAllDiagonels() {
+  // let quantityOfDiagonals = gameState.rowsQuantity + gameState.columnsQuantity - 1;
+  let result = false;
+
+  //minor  diagonals checking
+  for (let i = 0; i <= 3; i++) {
+    for (let j = 0; j <= 2; j++) {
+      let one = gameState.board[i][j];
+      let two = gameState.board[i + 1][j + 1];
+      let three = gameState.board[i + 2][j + 2];
+      let four = gameState.board[i + 3][j + 3];
+      if (checkFourForWin(one, two, three, four)) {
+        result = true;
+      }
+    }
+  }
+
+  //major diagonals checking
+  for (
+    let i = gameState.columnsQuantity - 1;
+    i >= gameState.columnsQuantity - 4;
+    i--
+  ) {
+    for (let j = 0; j <= 2; j++) {
+      let one = gameState.board[i][j];
+      let two = gameState.board[i - 1][j + 1];
+      let three = gameState.board[i - 2][j + 2];
+      let four = gameState.board[i - 3][j + 3];
+      if (checkFourForWin(one, two, three, four)) {
         result = true;
       }
     }
@@ -104,13 +170,16 @@ function checkAllRows() {
   return result;
 }
 
-function checkAllDiagonels() {}
-
 function checkFourForWin(one, two, three, four) {
-  if(one === undefined || two === undefined || three === undefined || four === undefined) {
+  if (
+    one === undefined ||
+    two === undefined ||
+    three === undefined ||
+    four === undefined
+  ) {
     return false;
   }
-  return (one === two) && (one === three) && (one === four);
+  return one === two && one === three && one === four;
 }
 
 function changePlayersTurn() {
@@ -118,5 +187,17 @@ function changePlayersTurn() {
 }
 
 function freezeTheGame() {
-  console.log('game is freezing');
+  gameState.isGameFinished = true;
+}
+
+function resetButtonClick() {
+  cleanDOM();
+  initiateGame();
+}
+
+function cleanDOM() {
+  document.querySelectorAll('.row').forEach(cell => {
+    cell.classList.remove(gameState.players[0]);
+    cell.classList.remove(gameState.players[1]);
+  })
 }
